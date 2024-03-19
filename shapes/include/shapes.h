573fdce34c02371d3df48ce72ee482e5bdf4a212
sergiip <svpuzyrev@gmail.com>
2024-03-19 14:15:17 +0200

    [Inheritance] Added example of inheritance of shape.
    Topics:
     - inheritance
     - virtual methods
     - overriding methods

---
+++ shapes/include/shapes.h
@@ -0,0 +1,102 @@
+﻿#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <exception>
+
+namespace shapes
+{
+
+    class Shape2D {
+    protected:
+        int x;
+        int y;
+    public:
+        Shape2D() = default;
+
+        Shape2D(int x_, int y_) : x(x_), y(y_) {}
+
+        int getX() const {
+            return x;
+        }
+
+        void setX(int x) {
+            Shape2D::x = x;
+        }
+
+        int getY() const {
+            return y;
+        }
+
+        void setY(int y) {
+            Shape2D::y = y;
+        }
+        //VMT
+        virtual double area() const = 0;
+
+        virtual double perimeter() const = 0;
+    };
+
+    class Circle: public Shape2D
+    {
+    private:
+        int radius;
+    public:
+
+        Circle() {}
+
+        Circle(int x, int y, int radius) : Shape2D(x, y), radius(radius) {}
+
+        int getRadius() const {
+            return radius;
+        }
+
+        void setRadius(int radius) {
+            Circle::radius = radius;
+        }
+
+        double area() const override {
+            return M_1_PI * radius * radius;
+        }
+
+        double perimeter() const override {
+            return 2* M_1_PI * radius;
+        }
+
+    };
+
+    class Rectangle: public Shape2D
+    {
+    private:
+        int w;
+        int h;
+    public:
+        Rectangle() {}
+
+        Rectangle(int x, int y, int w, int h) : Shape2D(x, y), w(w), h(h) {}
+
+        int getW() const {
+            return w;
+        }
+
+        void setW(int w) {
+            Rectangle::w = w;
+        }
+
+        int getH() const {
+            return h;
+        }
+
+        void setH(int h) {
+            Rectangle::h = h;
+        }
+
+        double area() const override {
+            return w*h;
+        }
+
+        double perimeter() const override {
+            return 2 *(w + h);
+        }
+    };
+}
---
+++ shapes/src/shapes_app.cpp
@@ -0,0 +1,35 @@
+﻿#include "shapes.h"
+
+using namespace std;
+
+using namespace shapes;
+
+int main()
+{
+
+    {
+        cout << "--- Circle ---" << endl;
+
+        Circle circle(100, 100, 200);
+
+        Shape2D& s = circle;
+
+        cout << s.area() << endl;
+
+        cout << s.perimeter()<< endl;
+    }
+
+    {
+        cout << "--- Rectangle ---" << endl;
+        Rectangle r{200, 300, 15, 10};
+
+        Shape2D& s2 = r;
+
+        cout << s2.area() << endl;
+
+        cout << s2.perimeter()<< endl;
+    }
+
+
+    return 0;
+}
